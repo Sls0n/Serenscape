05db87c3e324ba315f63851c7cfb48c09f2e6038
Silson sapkota <silson007@gmail.com>
2023-03-08 01:23:00 +0545

    set auth persistence to local storage

--- src/pages/SignUpPage/SignUp.jsx
+++ src/pages/SignUpPage/SignUp.jsx
@@ -3,13 +3,15 @@ import classes from './SignUp.module.scss';
 
 import SignUpHeader from './SignUpHeader';
 import SignUpForm from './SignUpForm';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, browserLocalPersistence } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
 const SignUpPage = () => {
   const navigate = useNavigate();
 
   const auth = getAuth();
+
+  auth.setPersistence(browserLocalPersistence);
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
