34b1a53eafad3be7a433ed0316bd255f60ca9419
Silson sapkota <silson007@gmail.com>
2023-05-10 19:12:46 +0545

    Commence migration to react-query

--- src/components/Section/Main/MainSounds/MainSoundList.jsx
+++ src/components/Section/Main/MainSounds/MainSoundList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import MainSound from './MainSound';
 import MainSoundSkeleton from './MainSoundSkeleton';
@@ -7,54 +7,46 @@ import classes from './MainSoundList.module.scss';
 
 import { db } from '../../../../config/firebase-config';
 import { getDocs, collection } from 'firebase/firestore';
+import { useQuery } from '@tanstack/react-query';
 
 const MainSoundList = () => {
-  const [sounds, setSounds] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const getSounds = async () => {
+    const data = await getDocs(collection(db, 'sounds'));
+    const soundsData = data.docs.map((doc) => doc.data());
 
-  useEffect(() => {
-    const getSounds = async () => {
-      try {
-        setIsLoading(true);
-        const data = await getDocs(collection(db, 'sounds'));
-        const soundsData = data.docs.map((doc) => doc.data());
-        setSounds(soundsData);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    return soundsData;
+  };
 
-    getSounds();
-  }, []);
+  const { data, isLoading, isError, error } = useQuery(['sounds'], getSounds);
+
+  if (isLoading) {
+    return <MainSoundSkeleton />;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
-    <>
-      {isLoading ? (
-        <MainSoundSkeleton />
-      ) : (
-        <ul className={classes['main__sounds']}>
-          {sounds.map(
-            (sound) =>
-              sound.imageSource &&
-              sound.title &&
-              sound.audioSource &&
-              sound.id && (
-                <MainSound
-                  key={sound?.id}
-                  imageSource={sound?.imageSource}
-                  title={sound?.title}
-                  audioSource={sound?.audioSource}
-                  pfp={sound?.pfp}
-                  artist={sound?.artist}
-                  id={sound?.id}
-                />
-              )
-          )}
-        </ul>
+    <ul className={classes['main__sounds']}>
+      {data.map(
+        (sound) =>
+          sound.imageSource &&
+          sound.title &&
+          sound.audioSource &&
+          sound.id && (
+            <MainSound
+              key={sound?.id}
+              imageSource={sound?.imageSource}
+              title={sound?.title}
+              audioSource={sound?.audioSource}
+              pfp={sound?.pfp}
+              artist={sound?.artist}
+              id={sound?.id}
+            />
+          )
       )}
-    </>
+    </ul>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
