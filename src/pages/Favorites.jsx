286922856033aa567803817c1bb3043575611ee8
Silson sapkota <silson007@gmail.com>
2023-05-20 13:52:14 +0545

    Insert MainTitle/Header in Favorites page comp

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Section from '../components/Section/Section';
 import Header from '../components/Header/Header';
 import FavoriteSoundList from '../components/Section/Main/MainSounds/FavoriteSoundList';
+import MainTitle from '../components/Section/Main/MainTitle';
 
 const FavoritesPage = () => {
   return (
     <Section>
       <Header firstNav={'Serenscape'} secondNav={'Favorites'} />
+      <MainTitle title={'My Favorites'} />
 
       <FavoriteSoundList />
     </Section>
